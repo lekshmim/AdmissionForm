a5e2c9340ecc9122d5190e453b89a185bd64a8ef
lekshmim <mnair.lekshmi@gmail.com>
2013-11-25 21:30:24 +0300

    First Commit

---
+++ SimpleAdmitionForm/Form1.Designer.vb
@@ -0,0 +1,328 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.butSubmit = New System.Windows.Forms.Button()
+        Me.butClear = New System.Windows.Forms.Button()
+        Me.butAdd = New System.Windows.Forms.Button()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.lisSelected = New System.Windows.Forms.ListBox()
+        Me.lisAct = New System.Windows.Forms.ListBox()
+        Me.CheckBox4 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox3 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.cmbcourse = New System.Windows.Forms.ComboBox()
+        Me.cmbfac = New System.Windows.Forms.ComboBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(86, 346)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(464, 108)
+        Me.ListBox1.TabIndex = 45
+        '
+        'butSubmit
+        '
+        Me.butSubmit.Location = New System.Drawing.Point(268, 303)
+        Me.butSubmit.Name = "butSubmit"
+        Me.butSubmit.Size = New System.Drawing.Size(75, 23)
+        Me.butSubmit.TabIndex = 44
+        Me.butSubmit.Text = "Submit"
+        Me.butSubmit.UseVisualStyleBackColor = True
+        '
+        'butClear
+        '
+        Me.butClear.Location = New System.Drawing.Point(349, 230)
+        Me.butClear.Name = "butClear"
+        Me.butClear.Size = New System.Drawing.Size(75, 23)
+        Me.butClear.TabIndex = 43
+        Me.butClear.Text = "Clear"
+        Me.butClear.UseVisualStyleBackColor = True
+        '
+        'butAdd
+        '
+        Me.butAdd.Location = New System.Drawing.Point(346, 201)
+        Me.butAdd.Name = "butAdd"
+        Me.butAdd.Size = New System.Drawing.Size(75, 23)
+        Me.butAdd.TabIndex = 42
+        Me.butAdd.Text = "ADD"
+        Me.butAdd.UseVisualStyleBackColor = True
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(444, 167)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(49, 13)
+        Me.Label8.TabIndex = 41
+        Me.Label8.Text = "Selected"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(230, 167)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(41, 13)
+        Me.Label7.TabIndex = 40
+        Me.Label7.Text = "Activity"
+        '
+        'lisSelected
+        '
+        Me.lisSelected.FormattingEnabled = True
+        Me.lisSelected.Location = New System.Drawing.Point(430, 186)
+        Me.lisSelected.Name = "lisSelected"
+        Me.lisSelected.Size = New System.Drawing.Size(120, 82)
+        Me.lisSelected.TabIndex = 39
+        '
+        'lisAct
+        '
+        Me.lisAct.FormattingEnabled = True
+        Me.lisAct.Location = New System.Drawing.Point(220, 186)
+        Me.lisAct.Name = "lisAct"
+        Me.lisAct.Size = New System.Drawing.Size(120, 82)
+        Me.lisAct.TabIndex = 38
+        '
+        'CheckBox4
+        '
+        Me.CheckBox4.AutoSize = True
+        Me.CheckBox4.Location = New System.Drawing.Point(408, 117)
+        Me.CheckBox4.Name = "CheckBox4"
+        Me.CheckBox4.Size = New System.Drawing.Size(39, 17)
+        Me.CheckBox4.TabIndex = 37
+        Me.CheckBox4.Text = "Pg"
+        Me.CheckBox4.UseVisualStyleBackColor = True
+        '
+        'CheckBox3
+        '
+        Me.CheckBox3.AutoSize = True
+        Me.CheckBox3.Location = New System.Drawing.Point(247, 117)
+        Me.CheckBox3.Name = "CheckBox3"
+        Me.CheckBox3.Size = New System.Drawing.Size(78, 17)
+        Me.CheckBox3.TabIndex = 36
+        Me.CheckBox3.Text = "Graduation"
+        Me.CheckBox3.UseVisualStyleBackColor = True
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(347, 117)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(48, 17)
+        Me.CheckBox2.TabIndex = 35
+        Me.CheckBox2.Text = "HSC"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(184, 117)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(47, 17)
+        Me.CheckBox1.TabIndex = 34
+        Me.CheckBox1.Text = "SSC"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(268, 82)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(59, 17)
+        Me.RadioButton2.TabIndex = 33
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "Female"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(182, 82)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(48, 17)
+        Me.RadioButton1.TabIndex = 32
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Male"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'cmbcourse
+        '
+        Me.cmbcourse.FormattingEnabled = True
+        Me.cmbcourse.Location = New System.Drawing.Point(385, 40)
+        Me.cmbcourse.Name = "cmbcourse"
+        Me.cmbcourse.Size = New System.Drawing.Size(121, 21)
+        Me.cmbcourse.TabIndex = 31
+        '
+        'cmbfac
+        '
+        Me.cmbfac.FormattingEnabled = True
+        Me.cmbfac.Location = New System.Drawing.Point(182, 40)
+        Me.cmbfac.Name = "cmbfac"
+        Me.cmbfac.Size = New System.Drawing.Size(121, 21)
+        Me.cmbfac.TabIndex = 30
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(182, 5)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(271, 20)
+        Me.TextBox1.TabIndex = 29
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.Location = New System.Drawing.Point(94, 167)
+        Me.Label6.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(114, 16)
+        Me.Label6.TabIndex = 28
+        Me.Label6.Text = "Optional Activities"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(94, 117)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(68, 16)
+        Me.Label5.TabIndex = 27
+        Me.Label5.Text = "Education"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(94, 82)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(53, 16)
+        Me.Label4.TabIndex = 26
+        Me.Label4.Text = "Gender"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(327, 43)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(51, 16)
+        Me.Label3.TabIndex = 25
+        Me.Label3.Text = "Course"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(94, 40)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(51, 16)
+        Me.Label2.TabIndex = 24
+        Me.Label2.Text = "Faculty"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(94, 5)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(45, 16)
+        Me.Label1.TabIndex = 23
+        Me.Label1.Text = "Name"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(637, 459)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.butSubmit)
+        Me.Controls.Add(Me.butClear)
+        Me.Controls.Add(Me.butAdd)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.lisSelected)
+        Me.Controls.Add(Me.lisAct)
+        Me.Controls.Add(Me.CheckBox4)
+        Me.Controls.Add(Me.CheckBox3)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.cmbcourse)
+        Me.Controls.Add(Me.cmbfac)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents butSubmit As System.Windows.Forms.Button
+    Friend WithEvents butClear As System.Windows.Forms.Button
+    Friend WithEvents butAdd As System.Windows.Forms.Button
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents lisSelected As System.Windows.Forms.ListBox
+    Friend WithEvents lisAct As System.Windows.Forms.ListBox
+    Friend WithEvents CheckBox4 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox3 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox2 As System.Windows.Forms.CheckBox
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents RadioButton2 As System.Windows.Forms.RadioButton
+    Friend WithEvents RadioButton1 As System.Windows.Forms.RadioButton
+    Friend WithEvents cmbcourse As System.Windows.Forms.ComboBox
+    Friend WithEvents cmbfac As System.Windows.Forms.ComboBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
---
+++ SimpleAdmitionForm/Form1.vb
@@ -0,0 +1,77 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        cmbfac.Items.Clear()
+        cmbfac.Items.Add("Hardware/Networking")
+        cmbfac.Items.Add("Software programming")
+        cmbfac.Items.Add("Animation")
+        lisAct.Items.Add("English Speaking")
+        lisAct.Items.Add("Personality Devlopement")
+        lisAct.Items.Add("MS-CIT")
+    End Sub
+
+    Private Sub cmbfac_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbfac.SelectedIndexChanged
+        If cmbfac.Text = "Hardware/Networking" Then
+            cmbcourse.Items.Clear()
+
+            cmbcourse.Items.Add("Computer hardware")
+            cmbcourse.Items.Add("Laptop")
+            cmbcourse.Items.Add("Lan connection")
+        End If
+        If cmbfac.Text = "Software programming" Then
+            cmbcourse.Items.Clear()
+
+            cmbcourse.Items.Add("C")
+            cmbcourse.Items.Add("C++")
+            cmbcourse.Items.Add("JAVA")
+        End If
+        If cmbfac.Text = "Animation" Then
+
+
+            cmbcourse.Items.Clear()
+
+            cmbcourse.Items.Add("2D")
+            cmbcourse.Items.Add("3D")
+
+        End If
+    End Sub
+
+    Private Sub cmbcourse_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles cmbcourse.SelectedIndexChanged
+           End Sub
+
+    Private Sub butClear_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles butClear.Click
+        lisSelected.Items.Clear()
+    End Sub
+
+    Private Sub butSubmit_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles butSubmit.Click
+        ListBox1.Items.Add("name:" + TextBox1.Text)
+        ListBox1.Items.Add("faculty:" + cmbfac.Text)
+
+        ListBox1.Items.Add("Course:" + cmbcourse.Text)
+
+        If RadioButton1.Checked Then
+            ListBox1.Items.Add("Gender:Male")
+        Else
+            ListBox1.Items.Add("Gender:Female")
+        End If
+        If CheckBox1.Checked Then
+            ListBox1.Items.Add("Education: ssc")
+        End If
+        If CheckBox1.Checked Then
+            ListBox1.Items.Add("Education: HSC")
+        End If
+        If CheckBox1.Checked Then
+            ListBox1.Items.Add("Education: Graduation")
+        End If
+        If CheckBox1.Checked Then
+            ListBox1.Items.Add("Education: Pg")
+        End If
+
+        ListBox1.Items.Add("Activity:" + lisSelected.Text)
+
+    End Sub
+
+    Private Sub butAdd_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles butAdd.Click
+        lisSelected.Items.Add(lisAct.SelectedItem)
+    End Sub
+End Class
